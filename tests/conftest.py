1b70fd0301f4163c0466f88a0aecb8323f9deab6
atom-tr <thai.tr@hotmail.com>
2024-11-09 17:05:11 +0700

    add path

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,5 @@
 import sys
 import os
 
-try:
-    pass
-except ImportError:
-    # Add the parent directory of the current file to the path
-    sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
+# Add the parent directory of the current file to the path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
